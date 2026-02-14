0a44b1804650e598001698331d121f669a2884f4
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2026-02-14 20:14:08 +0100

    Init frontend and backend skeleton

---
+++ backend/src/test/java/dev/rabauer/lille/chat/backend/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package dev.rabauer.lille.chat.backend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
---
+++ frontend/src/test/java/dev/rabauer/lille_chat/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rabauer.lille_chat.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
